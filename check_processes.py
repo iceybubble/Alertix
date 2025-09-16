41c699367e15003cc802516ab1c5e2a8552b36bb
Pallavi Kathait <kathaitpallavi164@gmail.com>
2025-09-16 12:44:59 +0530

    deleted unnecessary files

--- check_processes.py
+++
@@ -1,7 +0,0 @@
-import psutil
-
-for proc in psutil.process_iter(['name']):
-    try:
-        print(proc.info['name'])
-    except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
-        pass
